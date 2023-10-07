4a1440a9d8afcf1b6ee99fb2c0a3198e3fea3614
engalar <engalar@gmail.com>
2023-10-07 09:59:58 +0800

    rename

---
+++ src/TinySnippet/widget/TinySnippetContext.js
@@ -0,0 +1,8 @@
+define([
+    "dojo/_base/declare", "./TinySnippet"
+], function (declare, TinySnippet) {
+    "use strict";
+
+    // Declare widget"s prototype.
+    return declare("TinySnippet.widget.TinySnippetContext", [TinySnippet]);
+});
