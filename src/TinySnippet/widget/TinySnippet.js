38057e2b0aceaf097263b89dc87370c4835e8142
engalar <engalar@gmail.com>
2023-10-07 10:33:31 +0800

    clean

--- src/TinySnippet/widget/TinySnippet.js
+++ src/TinySnippet/widget/TinySnippet.js
@@ -1,28 +1,19 @@
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
-    "dojo/dom-style",
-    "dojo/dom-attr",
     "dojo/dom-construct",
     "dojo/_base/lang",
-    "dojo/html",
-    "dijit/layout/LinkPane"
 ], function (
     declare,
     _WidgetBase,
-    domStyle,
-    domAttr,
     domConstruct,
     lang,
-    html,
-    LinkPane
 ) {
     "use strict";
 
     return declare("TinySnippet.widget.TinySnippet", [_WidgetBase], {
         // Set in Modeler
         contents: "",
-        documentation: "",
         refreshOnContextChange: false,
         refreshOnContextUpdate: false,
 
@@ -37,7 +28,6 @@ define([
         },
 
         update: function (obj, callback) {
-            console.debug(this.id + ".update");
             this.contextObj = obj;
             if (this.refreshOnContextChange) {
                 this.evalJs();
@@ -61,7 +51,6 @@ define([
         },
 
         evalJs: function () {
-            console.debug(this.id + ".evalJS");
             try {
                 eval(this.contents + "\r\n//# sourceURL=" + this.id + ".js");
             } catch (error) {
@@ -70,7 +59,6 @@ define([
         },
 
         _handleError: function (error) {
-            console.debug(this.id + "._handleError");
             domConstruct.place(
                 '<div class="alert alert-danger">Error while evaluating javascript input: ' +
                 error +
@@ -81,7 +69,6 @@ define([
         },
 
         _executeCallback: function (cb, from) {
-            console.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
             if (cb && typeof cb === "function") {
                 cb();
             }
